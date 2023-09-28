e8ded7484dcd4c90db36d6959ece9a63cf4ddde6
crackedbeefcake <beef@vivalence.org>
2023-09-28 14:23:18 +0200

    itterating on backend build bc prisma not getting generatied

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+docker build -t v-backend ./backend
+
+docker run -e DATABASE_URL="postgresql://admin:password@host.docker.internal:5432/valence-spanish" v-backend
 docker run -e DATABASE_URL="postgresql://admin:password@host.docker.internal:5432/valence-spanish" v-backend
 
 docker run -e PUBLIC_GQL_SERVER_API_URL="http://host.docker.internal:4000" v-frontend
