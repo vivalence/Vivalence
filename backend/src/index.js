6c76346fe68e6cebfffe375dbaf69f5c7614923d
finn <finn@cofenster.com>
2023-09-29 19:01:19 +0200

    moved to exporess server cause apollo cors.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,32 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 import { prisma } from "./prisma-client.js";
 import { schema } from "./graphql/index.js";
 
-const apolloServer = new ApolloServer({ schema: schema });
+const PORT = process.env.PORT || 4000;
 
-const { url } = await startStandaloneServer(apolloServer, {
-    context: () => ({ prisma }),
-    listen: { port: 4000 }
+const app = express();
+const httpServer = http.createServer(app);
+
+const apolloServer = new ApolloServer({
+    schema: schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+await apolloServer.start();
+
+const apolloMiddlewear = expressMiddleware(apolloServer, {
+    context: async ({ req }) => ({ prisma, token: req.headers.token })
+});
+
+app.use("/", cors(), bodyParser.json(), apolloMiddlewear);
+
+await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
