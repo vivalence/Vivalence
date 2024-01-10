381937edbff200173145f5bf5cb9244e118930e8
finn <finn@cofenster.com>
2024-01-10 13:13:37 +0100

    i fucking hate fucking shit ass python

--- services/nlp/prod.sh
+++ services/nlp/prod.sh
@@ -3,9 +3,13 @@
 poetry --version
 poetry install --no-root
 
-echo "Pre assign PORT: $PORT"
-PORT=5050
-echo "Running on PORT: $PORT"
+poetry show
+
+poetry env info
+
+# echo "Pre assign PORT: $PORT"
+# PORT=5050
+# echo "Running on PORT: $PORT"
 
 # python -c 'import stanza; stanza.download("es")'
 poetry run python src/index.py $PORT 
