e6e5104ee75c7838c054bd3b02f07ab9a18a0c42
crackedbeefcake <beef@vivalence.org>
2024-01-10 13:37:00 +0100

    fuck python

--- services/nlp/prod.sh
+++ services/nlp/prod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# i fucking hate HATTEEEE!!! python
 
-poetry --version
+# poetry --version
 poetry install --no-root
 
 # stanza = "^1.7.0"
@@ -8,19 +9,21 @@ poetry install --no-root
 # uvicorn = "^0.25.0"
 # pydantic = "^2.5.3"
 
-poetry add stanza
-poetry add fastapi
-poetry add uvicorn
-poetry add pydantic
+source /app/.venv/bin/activate
 
-poetry show
+pip install stanza
+pip install fastapi
+pip install uvicorn
+pip install pydantic
 
-poetry env info
+# poetry show
+
+# poetry env info
 
 # echo "Pre assign PORT: $PORT"
 # PORT=5050
 # echo "Running on PORT: $PORT"
 
 # python -c 'import stanza; stanza.download("es")'
-poetry run python src/index.py $PORT 
+python src/index.py $PORT 
 
