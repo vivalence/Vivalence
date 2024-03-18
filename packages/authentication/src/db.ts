477afd5ccd47354c0a861170302b535970dcde9d
crackedbeefcake <beef@vivalence.org>
2024-03-18 18:53:29 +0100

    wip

--- packages/authentication/src/db.ts
+++ packages/authentication/src/db.ts
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/dynamic/public";
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/dynamic/public";
+import { env } from "$env/dynamic/public";
 
-export const supabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
+console.log("env", env);
+
+export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
