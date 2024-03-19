3dd7d35e3a6d4b3f426a2fdb202ae21fa8616ce5
crackedbeefcake <beef@vivalence.org>
2024-03-19 16:43:23 +0100

    wip

--- packages/client/src/routes/+layout.server.js
+++ packages/client/src/routes/+layout.server.js
@@ -4,6 +4,9 @@ import { env } from "$env/dynamic/public";
 export const load = async ({ route, locals: { getSession }, ...params }) => {
     const session = await getSession();
 
+    console.log("session", session);
+    console.log("route", route);
+    console.log("env", env);
     if (!session && route.id !== "/auth")
         throw redirect(307, `${env.PUBLIC_VIVALENCE_AUTH_PATH}?redirect=${route.path}`);
 
