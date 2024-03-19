240933e5ca37860629ebc9ef746992276f996c25
crackedbeefcake <beef@vivalence.org>
2024-03-19 16:29:08 +0100

    wip

--- packages/client/src/routes/+layout.server.js
+++ packages/client/src/routes/+layout.server.js
@@ -1,13 +1,11 @@
 import { redirect } from "@sveltejs/kit";
 import { env } from "$env/dynamic/public";
 
-console.log("layout.server.js", env);
 export const load = async ({ route, locals: { getSession }, ...params }) => {
     const session = await getSession();
 
-    console.log("rendering layout.server.js", env, route);
     if (!session && route.id !== "/auth")
-        throw redirect(307, `${env.PUBLIC_VIVALENCE_AUTH_URL}?redirect=${route.path}`);
+        throw redirect(307, `${env.PUBLIC_VIVALENCE_AUTH_PATH}?redirect=${route.path}`);
 
     return { session };
 };
